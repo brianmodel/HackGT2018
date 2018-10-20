b42c753cbab293dbc5c9b45012c335ddd16728d2
Ishan Arya <aryaishn@gmail.com>
2018-10-20 19:36:20 -0400

    Finished tab changing ux

--- tests/keywordrequest.js
+++ tests/keywordrequest.js
@@ -1,13 +1,18 @@
-$.get('http://localhost:5000/keywords', {
+$.post('http://localhost:5000/keywords', {
     paragraph: "water"
 }, (keywords, status) => {
     console.log(keywords);
     console.log(status);
 });
 
-$.get('http://localhost:5000/keywords', {
+$.post('http://localhost:5000/keywords', {
     paragraph: "After worries about higher interest rates sparked a steep sell-off in early October and again on Thursday, the S&P 500 remains down 5 percent from its Sept. 20 record high close, with top-shelf stocks including Amazon.com Inc (AMZN.O), Alphabet Inc (GOOGL.O), Netflix Inc (NFLX.O) and Facebook Inc (FB.O) showing little of their vitality from recent years."
 }, (keywords, status) => {
     console.log(keywords);
     console.log(status);
 });
+
+$.ajax({
+    url: 'http://localhost:5000/keywords',
+
+})
